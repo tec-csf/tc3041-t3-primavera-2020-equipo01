5e5c2f6db7a6b4fd8de0d51f1fccb88853e43463
axgtz <axguti@gmail.com>
2020-04-22 02:45:03 -0500

    added locations and businesses component ts

--- frontend/src/app/businesses/form-businesses/form-businesses.component.ts
+++ frontend/src/app/businesses/form-businesses/form-businesses.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Businesses } from 'src/app/shared/models/businesses';
+import { environment } from 'src/environments/environment';
+import { Form, NgForm } from '@angular/forms';
+declare var swal: any;
 
 @Component({
   selector: 'app-form-businesses',
@@ -6,10 +12,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-businesses.component.scss']
 })
 export class FormBusinessesComponent implements OnInit {
-
-  constructor() { }
+  edit = false
+  business:Businesses = new Businesses();
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute 
+  ) {
+    this.route.paramMap.subscribe(paramMap => {
+			if (paramMap.has('id')) {
+			  this.http.get(environment.apiUrl + 'businesses/get/'+ paramMap.get('id')).subscribe((res:any) => {
+          if(res.success){
+            this.business =  res.data;
+            this.edit = true;
+          }
+			  })
+			};
+		  });
+   }
 
   ngOnInit() {
   }
 
+  submit(f:NgForm){
+    if(!f.valid){
+      swal("Form is not valid, check all fields");
+      return;
+    }
+    if (this.edit){
+      this.http.put(environment.apiUrl + 'businesses/update/'+ this.business._id, this.business).subscribe((res:any) => {
+        if(res.success){
+          swal("The business has been updated!")
+        }
+      })
+    }
+    else{
+      this.business._id = Math.floor(Math.random() * 100000000)
+      this.http.post(environment.apiUrl + 'businesses/add', this.business).subscribe((res:any) => {
+        if(res.success){
+          swal("The business has been added!")
+        }
+      })
+    }
+  }
 }
--- frontend/src/app/locations/form-locations/form-locations.component.ts
+++ frontend/src/app/locations/form-locations/form-locations.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Locations } from 'src/app/shared/models/locations';
+import { environment } from 'src/environments/environment';
+import { Form, NgForm } from '@angular/forms';
+declare var swal: any;
 
 @Component({
   selector: 'app-form-locations',
@@ -6,10 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-locations.component.scss']
 })
 export class FormLocationsComponent implements OnInit {
-
-  constructor() { }
+  edit = false
+  location:Locations = new Locations();
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute 
+  ) {
+    this.route.paramMap.subscribe(paramMap => {
+			if (paramMap.has('id')) {
+			  this.http.get(environment.apiUrl + 'locations/get/'+ paramMap.get('id')).subscribe((res:any) => {
+          if(res.success){
+            this.location =  res.data;
+            this.edit = true;
+          }
+			  })
+			};
+		  });
+   }
 
   ngOnInit() {
+  }
+
+  submit(f:NgForm){
+    if(!f.valid){
+      swal("Form is not valid, check all fields");
+      return;
+    }
+    if (this.edit){
+      this.http.put(environment.apiUrl + 'locations/update/'+ this.location._id, this.location).subscribe((res:any) => {
+        if(res.success){
+          swal("The location has been updated!")
+        }
+      })
+    }
+    else{
+      this.location._id = Math.floor(Math.random() * 100000000)
+      this.http.post(environment.apiUrl + 'locations/add', this.location).subscribe((res:any) => {
+        if(res.success){
+          swal("The location has been added!")
+        }
+      })
+    }
   }
 
 }
