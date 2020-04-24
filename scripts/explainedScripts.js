648394f2d360c912fac7afb890c4a5357da30188
axgtz <axguti@gmail.com>
2020-04-23 22:47:27 -0500

    added explain to facet and geonear

--- scripts/explainedScripts.js
+++ scripts/explainedScripts.js
@@ -46,3 +46,96 @@ db.explain().Businesses.aggregate([{$lookup: {from: "Cases", localField: "_id", 
     	}
        }, {"$project":{"name":1, "last_name":1, "total_new_vectors":{"$size":"$friendNet"}}}, {$sort:{"total_new_vectors":-1}}
     ] )
+
+// geoNear, shows the people in cases divided by genre, who are over 60 years old and within a 200km distance of the point
+db.explain().Cases.aggregate( [
+  {
+    '$geoNear': {
+      'near': {
+        'type': 'Point', 
+        'coordinates': [
+          99.1332, 19.4326
+        ]
+      }, 
+      'distanceField': 'dist.calculated', 
+      'maxDistance': 200000, 
+      'includeLocs': 'dist.coordinates', 
+      'spherical': true
+    }
+  }, {
+    '$match': {
+      'age': {
+        '$gte': 60
+      }
+    }
+  }, {
+    '$group': {
+      '_id': '$gender', 
+      'People': {
+        '$addToSet': '$last_name'
+      }
+    }
+  }, {
+    '$sort': {
+      'location': 1
+    }
+  }
+])
+
+
+// Facet, first part divides the people confirmed with covid19 by age group in cases, second part divides the confirmed cases by genre in alphabetical order and shows the complete names.
+
+db.explain().Cases.aggregate( 
+[
+  {
+    '$match': {
+      'isConfirmed': true
+    }
+  }, {
+    '$facet': {
+      'age': [
+        {
+          '$bucket': {
+            'groupBy': '$age', 
+            'boundaries': [
+              20, 30, 40, 50, 60, 70, 80
+            ], 
+            'default': 'other', 
+            'output': {
+              'count': {
+                '$sum': 1
+              }, 
+              'who': {
+                '$addToSet': {
+                  '$concat': [
+                    '$name', ' ', '$last_name'
+                  ]
+                }
+              }
+            }
+          }
+        }
+      ], 
+      'gender': [
+        {
+          '$sort': {
+            'name': 1
+          }
+        }, {
+          '$group': {
+            '_id': '$gender', 
+            'who': {
+              '$push': {
+                '$concat': [
+                  '$name', ' ', '$last_name'
+                ]
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
+])
+
+
